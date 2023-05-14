577e7a6e0105e11c1ad325926306b67fb243a45b
SEN AN <san@tradecafe.com>
2023-05-14 16:50:06 -0400

    iterate all tokens

--- getRateSet.js
+++ getRateSet.js
@@ -7,12 +7,20 @@ const getRateSet = async ({ token, isBtc }) => {
     tokenPairStrings.push(token + '_' + 'USDT')
     tokenPairStrings.push('BTC' + '_' + 'USDT')
     //
+    // init rates
+    const rates = []
+    //
     // get rates
     for (let i = 0; i < tokenPairStrings.length; i++) {
         let url = `https://www.superex.com/trade/${tokenPairStrings[i]}`
         let rate = await getSingleRate({ url })
         console.log(`url: ${url} rate: ${rate}`);
+        rates.push(rate)
     }
+    //
+    // return rates
+    rates.push(100 * rates[0] * rates[2] / rates[1])
+    return { token, rates }
     //
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import getRateSet from "./getRateSet.js"
 
-const btcTokens = ['ETH', '1INCH', 'AAVE', 'ACH', 'ADA', 'AGLD', 'ALCX', 'ATOM', 'ALGO', 'ALICE', 'ALPACA', 'AMP', 'ANKR', 'AERGO', 'AGIX', 'ANT', 'AVAX', 'AMB', 'BAL', 'BAT', 'CHZ', 'COMP', 'CRV', 'DASH', 'DOGE', 'DOT', 'ENJ', 'EOS', 'FIL', 'GRT', 'KNC', 'LINK', 'LRC', 'LTC', 'MANA', 'MKR', 'OKB', 'OMG', 'REN', 'TRX', 'UNI', 'XRP', 'YFI', 'ZRX'];
+// const btcTokens = ['ETH', '1INCH', 'AAVE', 'ACH', 'ADA', 'AGLD', 'ALCX', 'ATOM', 'ALGO', 'ALICE', 'ALPACA', 'AMP', 'ANKR', 'AERGO', 'AGIX', 'ANT', 'AVAX', 'AMB', 'BAL', 'BAT', 'CHZ', 'COMP', 'CRV', 'DASH', 'DOGE', 'DOT', 'ENJ', 'EOS', 'FIL', 'GRT', 'KNC', 'LINK', 'LRC', 'LTC', 'MANA', 'MKR', 'OKB', 'OMG', 'REN', 'TRX', 'UNI', 'XRP', 'YFI', 'ZRX'];
+
+const btcTokens = ['ATOM', 'AMP', 'AERGO', 'AMB'];
 
 const ethTokens = ['AAVE', 'CRV', 'DASH', 'DOGE', 'DOT', 'EOS', 'FIL', 'LINK', 'LTC', 'MANA', 'MKR', 'OKB', 'SNX', 'SUSHI', 'TRX', 'UNI', 'XRP', 'YFI', 'ZRX'];
 
 (async function () {
-  await getRateSet({ token: btcTokens[1], isBtc: true })
+  for (let i = 0; i < btcTokens.length; i++) {
+    let result = await getRateSet({ token: btcTokens[i], isBtc: true })
+    console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+    console.log('result is:')
+    console.log(`         : ${result.token}`)
+    console.log(`         : ${result.rates[3]}`)
+    console.log(`         : ${result.rates[0]}, ${result.rates[1]}, ${result.rates[2]}`)
+    console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+  }
 })()
